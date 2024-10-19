7db7d68bf14d7537c4e005574e1801b09ae8ad6b
Ilya Khadunou <sibor883@gmail.com>
2024-10-19 20:58:46 +0300

    New file Junit

---
+++ Lesson_14_junit_5/FactorialNumber.java
@@ -0,0 +1,19 @@
+package Lesson_14_junit_5;
+
+public class FactorialNumber {
+    public static long getFactorialNumber(int numb){
+        if(numb < 0){
+            throw new IllegalArgumentException("Number must be non-negative.");
+        }
+
+        int result = 1;
+        for(int i = 1; i <= numb; i++){
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFactorialNumber(25));
+    }
+}
